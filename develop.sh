5e9dbc454b2e681c8123eaa30558c9318bd94fbb
rdlucas2 <>
2021-01-30 23:52:09 -0500

    open ports on docker sh

--- develop.sh
+++ develop.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -it --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp elixir /bin/bash
+docker run -it --rm -p 8085:8085 -p 4001:4001 -p 4002:4002 -p 8081:80 -v "$PWD":/usr/src/myapp -w /usr/src/myapp elixir /bin/bash
